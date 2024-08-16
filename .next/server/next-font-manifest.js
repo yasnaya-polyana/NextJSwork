3cde3e023ac828aa82e23890611918de83fa276e
Oscar Linehan <oladmin@Oscars-MacBook-Air.local>
2024-08-16 02:47:46 +0100

    Deleted Duplicates

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/oladmin/Desktop/NextJS/nextjs-dashboard/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/c556ae4be4c9cfa8-s.p.woff2\",\"static/media/ebd7dc65a6ba3e83-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
